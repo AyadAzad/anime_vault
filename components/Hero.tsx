3d10fd92a52dea9f5b96c4b75a964d388d1804db
Ayad <ayadlawrani65@gmail.com>
2024-08-09 16:13:37 +0300

    Update the web.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,13 +11,13 @@ function Hero() {
           height={96}
           className="object-contain"
         />
-        <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
+        <h1 className="sm:text-6xl text-5xl text-black lg:max-w-lg font-bold leading-[120%]">
           Explore The <span className="red-gradient">Diverse Realms</span> of
           Anime Magic
         </h1>
       </div>
       <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
-        <Image src="/anime.png" alt="anime" fill className="object-contain" />
+        <Image src="/bg.png" alt="anime" fill className="object-contain bg-transparent" />
       </div>
     </header>
   );
